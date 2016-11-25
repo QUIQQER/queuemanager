e2afc93256eb55d5a17dcb3c0a50e8cbbc78f195
Patrick Müller <p.mueller@pcsg.de>
2016-11-25 10:43:24 +0100

    refactor: debug-informationen entfernt

--- ajax/getQueueServers.php
+++ ajax/getQueueServers.php
@@ -8,16 +8,6 @@
 QUI::$Ajax->registerFunction(
     'package_quiqqer_queuemanager_ajax_getQueueServers',
     function () {
-        // job test
-        $Job = new \QUI\QueueManager\QueueJob(
-            \QUI\QueueManager\Examples\ExampleWorker::getClass(),
-            array(
-                'string' => 'Ich bin ein fröhliches Honigkuchenkaninchen.'
-            )
-        );
-
-        $Job->queue();
-
         return \QUI\QueueManager\QueueManager::getAvailableQueueServers();
     },
     array(),
